c17ee3c0d59ec46649ef5e2cae92006a5e7cf44a
Aryan Chavan <aryan@aryansdomain.com>
2021-09-22 12:38:08 +0530

    add login with jwt

--- main-api/routes/auth.js
+++ main-api/routes/auth.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
 router.post("/register", async (req, res) => {
 	if (req.body.user) {
-		if (req.body.user.username && req.body.user.password && req.body.user.email) {
+		if (
+			req.body.user.username &&
+			req.body.user.password &&
+			req.body.user.email
+		) {
 			const { username, password } = req.body.user;
 			// checking if user exists
 			if (await User.findOne({ username })) {
@@ -36,6 +41,44 @@ router.post("/register", async (req, res) => {
 			res.statusCode = 400;
 			res.json({ message: "missing username, email and/or password" });
 			// return;
+		}
+	} else {
+		res.statusCode = 400;
+		res.json({ message: "missing user in body" });
+	}
+});
+
+router.post("/login", async (req, res) => {
+	if (req.body.user) {
+		if (req.body.user.username && req.body.user.password) {
+			const { username, password } = req.body.user;
+			// getting user form db
+			try {
+				const currentUser = await User.findOne({ username });
+				if (!currentUser) {
+					res.statusCode = 400;
+					res.json({ message: "incorrect username or password" });
+				} else {
+					if (await bcrypt.compare(password, currentUser.password)) {
+						// signing and sending token
+						const token = jwt.sign(
+							{ _id: currentUser._id },
+							process.env.TOKEN_SECRET
+						);
+						res.header("auth-token", token);
+						res.json({ loggedIn: true });
+					} else {
+						res.statusCode = 400;
+						res.json({ message: "incorrect username or password" });
+					}
+				}
+			} catch {
+				res.statusCode = 500;
+				res.json({ message: "database error" });
+			}
+		} else {
+			res.statusCode = 400;
+			res.json({ message: "missing username and/or password" });
 		}
 	} else {
 		res.statusCode = 400;
