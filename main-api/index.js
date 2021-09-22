206f31469a2f8c911c726023e7e052609115b44a
Aryan Chavan <aryan@aryansdomain.com>
2021-09-22 13:10:36 +0530

    add auth middlewares

--- main-api/index.js
+++ main-api/index.js
@@ -3,12 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
+const authMiddleware = require("./middlewares/auth");
+const authOnly = require("./middlewares/authOnly");
+
 const authRouter = require("./routes/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(authMiddleware);
 
 mongoose.connect(process.env.DB_STRING, () => {
 	console.log("connected to db");
---
+++ main-api/middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+module.exports = (req, res, next) => {
+	const token = req.headers["auth-token"];
+
+	if (!token) {
+		req.auth = { isAuthenticated: false };
+		next();
+	} else {
+		try {
+			const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+			req.auth = { isAuthenticated: true, user: verified._id };
+			next();
+		} catch {
+			req.auth = { isAuthenticated: false };
+			next();
+		}
+	}
+};
