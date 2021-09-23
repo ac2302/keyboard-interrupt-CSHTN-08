68f0f4c6eec9627ad7cb75885d7514ddff0a8a55
Aryan Chavan <aryan@aryansdomain.com>
2021-09-23 11:10:45 +0530

    add react template

---
+++ react-frontend/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+	return <div>helo</div>;
+}
+
+export default App;
---
+++ react-frontend/src/config.js
@@ -0,0 +1,7 @@
+const config = {
+	locations: {
+		backend: "http://localhost:3030",
+	},
+};
+
+module.exports = config;
