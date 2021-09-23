e90a4fc7e8d6143982f31265fbd5d69476b5cea9
Aryan Chavan <aryan@aryansdomain.com>
2021-09-23 13:42:22 +0530

    ad basic routing

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,15 +1,26 @@
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Navbar />
 			<div className="main-scroller"></div>
-			<main></main>
+			<main>
+				<Switch>
+					<Route path="/" exact>
+						welcome
+					</Route>
+					<Route path="/roadmap" exact>
+						roadmap
+					</Route>
+				</Switch>
+			</main>
 			<Footer />
-		</>
+		</Router>
 	);
 }
 
