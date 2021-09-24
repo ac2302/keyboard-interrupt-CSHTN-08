85a02e57d15f04b7f53b7bba71382f1a06ca70cf
Aryan Chavan <aryan@aryansdomain.com>
2021-09-25 03:36:10 +0530

    add certificate

--- react-frontend/src/components/CertificatePage.jsx
+++ react-frontend/src/components/CertificatePage.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import config from "../config";
 
+import "./CertificatePage.css"
+
 function CertificatePage({ auth }) {
 	const [modules, setModules] = useState([]);
 	const [self, setSelf] = useState(false);
@@ -23,7 +25,37 @@ function CertificatePage({ auth }) {
 
 	return (
 		<div className="certificate-page">
-			{self.currentModule == modules.length && "sus"}
+			{self.currentModule == modules.length && <Certificate self={self} />}
+			<button
+				type="button"
+				className="btn btn-primary"
+				onClick={() => {
+					window.print();
+				}}
+			>
+				Download Certificate
+			</button>
+		</div>
+	);
+}
+
+function Certificate({ self }) {
+	return (
+		<div className="certificate" id="section-to-print">
+			<h1>Certificate Of Completion</h1>
+			<h3>Keyboard Interrupt Full Stack Web Dev (MERN Stack)</h3>
+
+			<p>
+				This is to certify that the individual{" "}
+				<b>
+					{self.name.first} {self.name.last}
+				</b>{" "}
+				with the email address <b>{self.email}</b> has completed the full stack
+				web development course offered by Keyboard Interrupt and now possesses
+				the skill and knowledge nessecary to function as a full stack MERN stack
+				developer.
+			</p>
+			<h4>Keyboad Interrupt</h4>
 		</div>
 	);
 }
