2473c74e9098f117f666f82c838296c954f7e019
Aryan Chavan <aryan@aryansdomain.com>
2021-09-24 10:59:21 +0530

    addd better video and modules view

--- react-frontend/src/components/WatchPage.jsx
+++ react-frontend/src/components/WatchPage.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import ReactPlayer from "react-player/lazy";
+import { FaPlay } from "react-icons/fa";
+import { AiOutlineClockCircle } from "react-icons/ai";
 
 import config from "../config";
+
+import "./WatchPage.css";
 
 function useQuery() {
 	return new URLSearchParams(useLocation().search);
@@ -33,13 +38,73 @@ function WatchPage({ auth }) {
 		<div>
 			{videoUrl && (
 				<>
-					<video autoPlay controls>
-						<source src={videoUrl} type="video/mp4" />
-						Your browser does not support the video tag.
-					</video>
+					<div className="video-container">
+						<ReactPlayer
+							playing
+							url={videoUrl}
+							controls
+							width="100%"
+							height="100%"
+						/>
+						<h2>{modules[module].lectures[lecture].title}</h2>
+						<p>{modules[module].lectures[lecture].description}</p>
+					</div>
+					<VideosList
+						auth={auth}
+						module={module}
+						lecture={lecture}
+						modules={modules}
+					/>
 				</>
 			)}
 		</div>
+	);
+}
+
+function VideosList({ auth, module, lecture, modules }) {
+	const [progress, setProgress] = useState(false);
+	useEffect(() => {
+		axios
+			.get(`${config.locations.backend}/user/progress/self`, {
+				headers: { "auth-token": auth },
+			})
+			.then((res) => {
+				setProgress(res.data);
+			});
+	}, []);
+
+	console.log({ progress });
+
+	return progress ? (
+		<div className="videos-list">
+			{modules.map((m) => (
+				<div className="module" key={m._id}>
+					<h4>{m.name}</h4>
+					{m.lectures.map((l, lessonIndex) => (
+						<div className="lesson" key={l._id}>
+							<a href={`/watch?module=${m.index}&lesson=${lessonIndex}`}>
+								<FaPlay
+									className={`icon ${
+										progress.currentModule > m.index ||
+										(progress.currentModule == m.index &&
+											progress.lastLesson >= lessonIndex)
+											? "completed"
+											: ""
+									}`}
+								/>
+							</a>
+							<span className="title">{l.title}</span>
+							<span className="duration">
+								<AiOutlineClockCircle className="clock-icon" />
+								{l.duration} min
+							</span>
+						</div>
+					))}
+				</div>
+			))}
+		</div>
+	) : (
+		""
 	);
 }
 
