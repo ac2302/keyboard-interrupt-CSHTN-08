add9b340c46f29f7cdcd75f6f68b8f5f7d00d012
Aryan Chavan <aryan@aryansdomain.com>
2021-09-25 08:16:03 +0530

    add api call

--- bots/progressbotfinal/progressbot.py
+++ bots/progressbotfinal/progressbot.py
@@ -2,10 +2,19 @@ import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+import requests
 
 client = commands.Bot(command_prefix='$')
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+
+
+def getProg(username):
+    response = requests.get(
+        f'http://localhost:3030/user/progress/by-username/{username}')
+    module = response.json()['currentModule'] + 1
+    lecture = response.json()['lastLesson'] + 2
+    return f"```module {module}, video {lecture}```"
 
 
 @client.event
@@ -16,8 +25,12 @@ async def on_ready():
 @client.command()
 async def progress(ctx, arg):
     if str(ctx.channel) == 'general':
-        await ctx.send(f"Profile:{arg}")
-        await ctx.send("Progress:")
+        try:
+            prog = getProg(arg)
+            await ctx.send(f"Profile: {arg}")
+            await ctx.send(f"Progress: {prog}")
+        except:
+            await ctx.send(f"invalid user: {arg}")
     else:
         await ctx.send("Wrong channel")
 
